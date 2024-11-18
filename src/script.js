b607a443fd43544eeee5a5bdfabdb5e6a450c2b3
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-18 23:12:46 +0100

    updated the current date and time on script.js

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
 // Function to update weather details
 function weatherDetail(response) {
-    let cityElement = document.querySelector("#current-city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity-details");
-    let windSpeedElement = document.querySelector("#speed-details");
-    let temperatureElement = document.querySelector("#current-temperature-value");
-    let weatherIconElement = document.querySelector("#weather-icon");
-    let currentDateElement = document.querySelector("#current-date");
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity-details");
+  let windSpeedElement = document.querySelector("#speed-details");
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let weatherIconElement = document.querySelector("#weather-icon");
+  let currentDateElement = document.querySelector("#current-date");
 
-    // Fetch data from the API response
-    let temperature = Math.round(response.data.main.temp);
-    let cityName = response.data.name;
-    let description = response.data.weather[0].description;
-    let humidity = response.data.main.humidity;
-    let windSpeed = response.data.wind.speed;
-    let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  // Fetch data from the API response
+  let temperature = Math.round(response.data.main.temp);
+  let cityName = response.data.name;
+  let description = response.data.weather[0].description;
+  let humidity = response.data.main.humidity;
+  let windSpeed = response.data.wind.speed;
+  let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 
-    // Update elements with API data
-    cityElement.innerHTML = cityName;
-    descriptionElement.innerHTML = description;
-    humidityElement.innerHTML = `${humidity}%`;
-    windSpeedElement.innerHTML = `${windSpeed} km/h`;
-    temperatureElement.innerHTML = temperature;
-    weatherIconElement.innerHTML = `<img src="${iconUrl}" alt="${description}" class="weather-app-icon" />`;
+  // Update elements with API data
+  cityElement.innerHTML = cityName;
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windSpeedElement.innerHTML = `${windSpeed} km/h`;
+  temperatureElement.innerHTML = temperature;
+  weatherIconElement.innerHTML = `<img src="${iconUrl}" alt="${description}" class="weather-app-icon" />`;
 
-
-
-
-
+  // Get and display current date and time
+  let now = new Date();
+  currentDateElement.innerHTML = formatDate(now);
+}
