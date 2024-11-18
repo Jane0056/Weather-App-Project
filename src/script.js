874141e25932b9081e808846a9eab7c11e2aa4b4
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-18 22:13:41 +0100

    Added the API integration to the script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+function weatherDetail(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "0c0fc4d0af9a25bbb3ad3644ab6e153c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(weatherDetail);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
@@ -7,3 +23,5 @@ function search(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Berlin");
