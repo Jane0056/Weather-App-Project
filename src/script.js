adee9c6868d3fe4b8a32966d883b2ac2d0e06303
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-18 23:09:35 +0100

    updated the  Function to update weather details in script.js

--- src/script.js
+++ src/script.js
@@ -1,22 +1,16 @@
+// Function to update weather details
 function weatherDetail(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#current-city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#Humditiy-details");
-  let windSpeedElement = document.querySelector("#speed-details");
-  let cityTimeElement = document.querySelector("#city-time");
-  let date = new Date(response.data.time * 1000);
-  let weatherIconElement = document.querySelector("#weather-icon");
-  weatherIconElement.innerHTML`<img src=${response.data.condition.icon_url}"class="weather-app-icon" />`;
-  cityElement.innerHTML = response.data.city;
-  cityTimeElement.innerHTML = formateDate;
+    let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity-details");
+    let windSpeedElement = document.querySelector("#speed-details");
+    let temperatureElement = document.querySelector("#current-temperature-value");
+    let weatherIconElement = document.querySelector("#weather-icon");
+    let currentDateElement = document.querySelector("#current-date");
 
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = math.round(temperature);
-}
+
+
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
