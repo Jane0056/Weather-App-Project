4f6ba06f1780fd45e2786dd3d835fbe1c6170b59
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-18 23:10:47 +0100

    Fetched data from the API response

--- src/script.js
+++ src/script.js
@@ -8,48 +8,15 @@ function weatherDetail(response) {
     let weatherIconElement = document.querySelector("#weather-icon");
     let currentDateElement = document.querySelector("#current-date");
 
+    // Fetch data from the API response
+    let temperature = Math.round(response.data.main.temp);
+    let cityName = response.data.name;
+    let description = response.data.weather[0].description;
+    let humidity = response.data.main.humidity;
+    let windSpeed = response.data.wind.speed;
+    let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 
 
 
 
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let day = days[date.getDay()];
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${day}${hours}:${minutes}`;
-}
-
-function searchCity(city) {
-  let apiKey = "0c0fc4d0af9a25bbb3ad3644ab6e153c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(weatherDetail);
-}
-
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", search);
-
-searchCity("Berlin");
