0c60f63d184fa4104f3f09c2af858d1280ec68b4
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-18 22:19:57 +0100

    added the weather data into the script.js

--- src/script.js
+++ src/script.js
@@ -2,8 +2,19 @@ function weatherDetail(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
-
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#Humditiy-details");
+  let windSpeedElement = document.querySelector("#speed-details");
+  let cityTimeElement = document.querySelector("#city-time");
+  let date = new Date(response.data.time * 1000);
+  let weatherIconElement = document.querySelector("#weather-icon");
+  weatherIconElement.innerHTML`<img src=${response.data.condition.icon_url}"class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
+  cityTimeElement.innerHTML = formateDate;
+
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = math.round(temperature);
 }
 
